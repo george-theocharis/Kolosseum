c45eddf386ca58672f62abfe4d645787912e1031
giorgosTh <giwrgos.theocharis@gmail.com>
2020-11-07 14:51:55 +0200

    Refactor rule of 3 violation
    The rule of 3 says that if something is repeated
    at least 3 times, is a duplication that needs
    refactoring.

--- src/main/kotlin/fizz_buzz/FizzBuzz.kt
+++ src/main/kotlin/fizz_buzz/FizzBuzz.kt
@@ -2,8 +2,6 @@ package fizz_buzz
 
 class FizzBuzz {
     operator fun invoke(number: Int): String {
-        if(number == 4) return "4"
-        if(number == 2) return "2"
-        return "1"
+       return number.toString()
     }
 }
