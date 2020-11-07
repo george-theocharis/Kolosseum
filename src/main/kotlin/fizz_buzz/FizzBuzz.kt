c6a4b511429a1ea87600a97bbfde72f539121e0d
giorgosTh <giwrgos.theocharis@gmail.com>
2020-11-07 15:11:45 +0200

    Refactor solution to a when expression

--- src/main/kotlin/fizz_buzz/FizzBuzz.kt
+++ src/main/kotlin/fizz_buzz/FizzBuzz.kt
@@ -1,10 +1,10 @@
 package fizz_buzz
 
 class FizzBuzz {
-    operator fun invoke(number: Int): String {
-        if (number % 3 == 0 && number % 5 == 0) return "FizzBuzz"
-        if (number % 3 == 0) return "Fizz"
-        if (number % 5 == 0) return "Buzz"
-        return number.toString()
+    operator fun invoke(number: Int) = when {
+        number % 3 == 0 && number % 5 == 0 -> "FizzBuzz"
+        number % 3 == 0 -> "Fizz"
+        number % 5 == 0 -> "Buzz"
+        else -> number.toString()
     }
 }
