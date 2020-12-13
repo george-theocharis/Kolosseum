e31517af048c762895687767f21028a1b0bae44a
giorgosTh <giwrgos.theocharis@gmail.com>
2020-12-13 12:33:38 +0200

    Cleanup tests
    Tests needs refactoring too so cleaned them up a bit.

--- src/test/kotlin/roman_numerals/RomanNumeralsTest.kt
+++ src/test/kotlin/roman_numerals/RomanNumeralsTest.kt
@@ -6,62 +6,27 @@ import org.junit.jupiter.api.Test
 class RomanNumeralsTest {
 
     @Test
-    fun `converting 1 should be "I"`() {
-
-        convert(1) shouldBe "I"
-    }
-
-    @Test
-    fun `converting 2 should be "II"`() {
-
-        convert(2) shouldBe "II"
-    }
-
-    @Test
-    fun `converting 3 should be "III"`() {
-
-        convert(3) shouldBe "III"
+    fun `converting 1 to 3 should be "I", "II", "III"`() {
+        (1..3).map { convert(it) } shouldBe listOf("I", "II", "III")
     }
 
     @Test
     fun `converting 4 should be "IV"`() {
-
         convert(4) shouldBe "IV"
     }
 
     @Test
     fun `converting 5 should be "V"`() {
-
-        convert(5) shouldBe "V"
-    }
-
-    @Test
-    fun `converting 6 should be "VI"`() {
-
-        convert(6) shouldBe "VI"
-    }
-
-    @Test
-    fun `converting 7 should be "VII"`() {
-
-        convert(7) shouldBe "VII"
-    }
-
-    @Test
-    fun `converting 8 should be "VIII"`() {
-
-        convert(8) shouldBe "VIII"
+        (5..8).map { convert(it) } shouldBe listOf("V", "VI", "VII", "VIII")
     }
 
     @Test
     fun `converting 9 should be "IX"`() {
-
         convert(9) shouldBe "IX"
     }
 
     @Test
     fun `converting 10 to 13 should be "X, XI, XII, XIII"`() {
-
         (10..13).map { convert(it) } shouldBe listOf("X", "XI", "XII", "XIII")
     }
 
@@ -72,7 +37,6 @@ class RomanNumeralsTest {
 
     @Test
     fun `converting 15 to 18 should be "XV, XVI, XVII, XVIII"`() {
-
         (15..18).map { convert(it) } shouldBe listOf("XV", "XVI", "XVII", "XVIII")
     }
 }
