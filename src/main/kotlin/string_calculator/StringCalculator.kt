6821a472f1667d61b05b7cbcb2a6d9adec049a89
giorgosTh <giwrgos.theocharis@gmail.com>
2020-12-16 17:54:11 +0200

    Remove check for contains as it is not needed any more

--- src/main/kotlin/string_calculator/StringCalculator.kt
+++ src/main/kotlin/string_calculator/StringCalculator.kt
@@ -7,12 +7,9 @@ class StringCalculator {
     fun add(numbers: String): Int {
         if (numbers.isEmpty()) return 0
         return try {
-            if (numbers.contains(',')) {
-                return numbers.split(',').fold(0) { sum: Int, splittedValue: String ->
-                     sum + splittedValue.toInt()
-                }
+            numbers.split(',').fold(0) { sum: Int, splittedValue: String ->
+                sum + splittedValue.toInt()
             }
-            numbers.toInt()
         } catch (e: NumberFormatException) {
             throw BadInputFormatException("Inappropriate input: $numbers")
         }
